cd47a0490cfb8fa9a5b64fdfffc801d60f596fa2
Tara Evans <tarajevans@hotmail.com>
2022-11-20 18:40:21 -0500

    remove logos and add animals

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -75,45 +75,46 @@ db.once("open", async () => {
       {
         name: "All Paws Matter",
         website: "https://www.allpawsmatter.ca/",
-        image: "logoallpawsfixed.png",
+        image: "charity-dog1.png",
         description: "All Paws Matter Dog Rescue is a small, Ontario based, non-profit organization that aids in the re-homing and rehabilitation of dogs.", 
     },
     {
       name: "Scugog Pet Food Bank",
       website: "https://www.facebook.com/UxScPetFoodBank/",
-      image: "Uxbridge-Scugog.jpg",
+      image: "charity-cat1.png",
       description: "Our mission is to keep families together by helping them feed their pets in North Durham Region (Ontario, Canada). When we have extra food & supplies we love helping other animal welfare organizations.",
-    },
-    {
-      name: "Team Chelsea",
-      website: "https://www.facebook.com/groups/165123396868018",
-      image: "team-chelsea.jpg",
-      description: "Team Chelsea provides assisstance for lost & found pets in Durham Region, Ontario",
     },
     {
       name: "Durham Humane Society",
       website: "http://www.hsdr.org/",
-      image: "HSDR-logo.png",
+      image: "charity-bunny1.png",
       description: "The Humane Society of Durham Region is a non-profit animal rescue organization.  It is dedicated to protecting and providing compassionate care for unwanted and abused animals in Durham Region until they find their forever homes.",
+    },
+    {
+      name: "Team Chelsea",
+      website: "https://www.facebook.com/groups/165123396868018",
+      image: "charity-dog2.png",
+      description: "Team Chelsea provides assisstance for lost & found pets in Durham Region, Ontario",
+    },
+    {
+      name: "The Pet Pantry",
+      website:"https://www.facebook.com/thepetpantryguelph/",
+      image: "charity-cat2.png",
+      description: "Guelph's Pet Food Bank",
     },
     {
       name: "Toronto Humane Society",
       website: "https://www.torontohumanesociety.com/",
-      image: "Toronto-logo.png",
+      image: "charity-bunny2.png",
       description: "If you love animals as much as we do, you understand why we work so hard to provide the best life possible for the animals in our care and the people who bond with them. It’s our expertise at every step of the journey, combined with the loving homes provided by people in our community that complete the circle. From the very moment an animal comes into our fold, their wellbeing is our priority.",
     },
     {
       name: "Jack Russell Terrier Rescue Ontario",
       website: "https://russellrescue.org/",
-      image: "Uxbridge-Scugog.jpg",
+      image: "charity-jrt.png",
       description: "Jack Russell Terrier Rescue Ontario (JRTRO) is a registered charity and referral organization dedicated to fostering, finding and placing Jack Russell Terriers (or “almost Jacks” aka Jack mixes) in new homes.",
     },
-    {
-      name: "The Pet Pantry",
-      website:"https://www.facebook.com/thepetpantryguelph/",
-      image: "Uxbridge-Scugog.jpg",
-      description: "Guelph's Pet Food Bank",
-    },
+
   ]);
 
   console.log('seeded');
