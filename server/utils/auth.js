44fad8f857604a9fd41ec7adf88d6aae1581e3b5
Joe Fisher <josephfish1973@gmail.com>
2022-11-26 16:24:24 -0500

    changed DB settings for AWS

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     // create token (aka 'sign' token)
     signToken: function ({ username, email, _id }) {
         const payload = { username, email, _id };
-        return jwt.sign({ data: payload, exp:Math.floor(Date.now() / 1000) + expiration }, process.env.TOKEN_SECRET);
+        return jwt.sign({ data: payload, exp:Math.floor(Date.now() / 1000) + expiration }, "secretsauce");//process.env.TOKEN_SECRET)
     },
     // authenticate token
     authMiddleware: function ({ req }) {
@@ -20,7 +20,7 @@ module.exports = {
         }
         // verify that the token matches a user
         try {
-            const { data } = jwt.verify(token, process.env.TOKEN_SECRET, { maxAge: expiration });
+            const { data } = jwt.verify(token, "secretsauce", { maxAge: expiration });
             req.user = data;
         } catch {
             console.log('Invalid token');
